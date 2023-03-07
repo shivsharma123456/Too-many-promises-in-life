09b4e0065216f2ccb111cd284904a8aacfc62b6e
Shiv Sharma <shiv.yogesh92@gmail.com>
2023-03-07 22:25:02 +0530

    Async Await

--- Promiseall.js
+++ Promiseall.js
@@ -1,28 +1,75 @@
-function updateLastUserActivityTime(user, posts, createPost, deletePost) {
-    const newPost = { id: posts.length + 1, userId: user.id, content: "New post" };
+// function updateLastUserActivityTime(user, posts, createPost, deletePost) {
+//     const newPost = { id: posts.length + 1, userId: user.id, content: "New post" };
   
-    const createPostPromise = createPost(newPost);
-    const updateActivityTimePromise = new Promise((resolve) => {
+//     const createPostPromise = createPost(newPost);
+//     const updateActivityTimePromise = new Promise((resolve) => {
+//       setTimeout(() => {
+//         user.lastActivityTime = new Date();
+//         resolve();
+//       }, 1000);
+//     });
+  
+//     Promise.all([createPostPromise, updateActivityTimePromise]).then(() => {
+//       console.log(`All posts by user ${user.id}:`);
+//       posts.filter((post) => post.userId === user.id).forEach((post) => console.log(post));
+//       console.log(`Last activity time of user ${user.id}: ${user.lastActivityTime}`);
+  
+//       const lastPost = posts[posts.length - 1];
+//       deletePost(lastPost).then(() => {
+//         console.log(`Posts of user ${user.id} after deleting last post:`);
+//         posts.filter((post) => post.userId === user.id).forEach((post) => console.log(post));
+//       });
+//     });
+  
+//     return Promise.all([createPostPromise, updateActivityTimePromise]);
+//   }
+  function getButter() {
+    return new Promise((resolve, reject) => {
       setTimeout(() => {
-        user.lastActivityTime = new Date();
-        resolve();
+        resolve('Butter');
+      }, 2000);
+    });
+  }
+  
+  function getBread() {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve('Bread');
+      }, 3000);
+    });
+  }
+  
+  function getColdDrinks() {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve('Cold Drinks');
       }, 1000);
     });
+  }
   
-    Promise.all([createPostPromise, updateActivityTimePromise]).then(() => {
-      console.log(`All posts by user ${user.id}:`);
-      posts.filter((post) => post.userId === user.id).forEach((post) => console.log(post));
-      console.log(`Last activity time of user ${user.id}: ${user.lastActivityTime}`);
+  async function makeSandwich() {
+    const butter = await getButter();
+    const bread = await getBread();
+    const coldDrinks = await getColdDrinks();
+    console.log(`Made a sandwich with ${butter} and ${bread}, and got ${coldDrinks} as well.`);
+  }
   
-      const lastPost = posts[posts.length - 1];
-      deletePost(lastPost).then(() => {
-        console.log(`Posts of user ${user.id} after deleting last post:`);
-        posts.filter((post) => post.userId === user.id).forEach((post) => console.log(post));
+  makeSandwich();
+  
+  
+  getButter()
+    .then(butter => {
+      return getBread().then(bread => {
+        return [butter, bread];
+      });
+    })
+    .then(([butter, bread]) => {
+      return getColdDrinks().then(coldDrinks => {
+        console.log(`Made a sandwich with ${butter} and ${bread}, and got ${coldDrinks} as well.`);
       });
     });
   
-    return Promise.all([createPostPromise, updateActivityTimePromise]);
-  }
- 
+  
+  
 
   
